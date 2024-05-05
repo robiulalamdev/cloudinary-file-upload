cf8dffde6549c61be2813b0e75f103d4b19581cb
Md. Robiul Alam <105594633+robiulalamdev@users.noreply.github.com>
2024-05-05 19:42:54 +0600

    Delete nextjs-node-mongoose directory

--- nextjs-node-mongoose/server/src/modules/storage/storage.route.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const { createFile } = require("./storage.controller");
-const router = express.Router();
-
-router.post("/create", createFile);
-
-module.exports = router;
